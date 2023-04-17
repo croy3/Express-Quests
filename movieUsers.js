dcbc5035ce1dfbd70f9c0141ea927600614f1cd5
croy3 <c.roy3728@gmail.com>
2023-04-17 12:09:34 +0200

    update GET

--- movieUsers.js
+++ movieUsers.js
@@ -1,10 +1,47 @@
 const database = require("./database");
 
-const getUsers = (req, res) => {
+/*const getUsers = (req, res) => {
     database
       .query("select * from users")
       .then(([users]) => {
         res.status(200).json(users);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
+  };*/
+
+  const getUsers = (req, res) => {
+    let initialSql = "select * from users"
+    const where = [];
+  
+    if (req.query.language != null) {
+      where.push({
+        column: "language",
+        value: req.query.language,
+        operator: "=",
+      });
+    }
+    if (req.query.city != null) {
+      where.push({
+        column: "city",
+        value: req.query.city,
+        operator: "=",
+      });
+    }
+  
+    database
+      .query(
+        where.reduce(
+          (sql, {column, operator}, index) =>
+          `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+          initialSql
+        ),
+        where.map(({ value }) => value)
+      )
+      .then(([users]) => {
+        res.json(users);
       })
       .catch((err) => {
         console.error(err);
